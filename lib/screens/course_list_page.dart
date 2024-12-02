5702941fd4b0ce46e3ae437ecf26262e96da3311
angelsusum <angelsusum@gmail.com>
2024-12-02 19:16:07 +0900

    Feat: 그룹 검색 기능

--- lib/screens/course_list_page.dart
+++ lib/screens/course_list_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:pettrip_fe/const/category.dart';
 import 'package:pettrip_fe/const/dummy_data.dart';
+import 'package:pettrip_fe/models/course_model.dart';
 import 'package:pettrip_fe/screens/upload_course_page.dart';
 import 'package:pettrip_fe/widgets/course_card.dart';
+import 'package:pettrip_fe/widgets/filter_modal.dart';
 
 import '../const/colors.dart';
 
@@ -14,23 +17,162 @@ class CourseListPage extends StatefulWidget {
 }
 
 class _CourseListPageState extends State<CourseListPage> {
+  // 검색 및 필터링 관련 상태 변수
+  String? _searchTitle; // 검색어
+  String? _selectedProvince; // 선택된 도/광역시
+  String? _selectedCity; // 선택된 시/군/구
+  List<String> _selectedTags = []; // 선택된 태그
+  bool _isFilterApplied = false; // 핕터 적용 여부
+
+  // TODO: 코스 목록 가져오는 로직 추가
+  List<CourseModel> _filteredCourses = dummyCourses; // 필터링 된 코스
+
+  // 코스 필터링 메서드
+  void _filterCourses() {
+    setState(() {
+      _filteredCourses = dummyCourses.where((course) {
+        final matchesTitle = _searchTitle == null ||
+            course.courseName
+                .toLowerCase()
+                .contains(_searchTitle!.toLowerCase());
+        final matchesLocation = (_selectedProvince == null ||
+                course.province == _selectedProvince) &&
+            (_selectedCity == null || course.city == _selectedCity);
+        final matchesTags = _selectedTags.isEmpty ||
+            _selectedTags.any((tag) => course.tags.contains(tag));
+
+        return matchesTitle && matchesLocation && matchesTags;
+      }).toList();
+    });
+  }
+
+// 필터 모달 호출
+  void _showFilterModal(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+      ),
+      builder: (context) {
+        return FilterModal(
+          selectedProvince: _selectedProvince,
+          selectedCity: _selectedCity,
+          selectedTags: _selectedTags,
+          tagList: courseTags,
+          onProvinceChanged: (province) {
+            setState(() {
+              _selectedProvince = province;
+            });
+          },
+          onCityChanged: (city) {
+            setState(() {
+              _selectedCity = city;
+            });
+          },
+          onTagsChanged: (tags) {
+            _selectedTags = tags;
+          },
+          onApply: () {
+            setState(() {
+              _isFilterApplied = true;
+            });
+            _filterCourses();
+          },
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(backgroundColor: Colors.white, title: Text('코스 찾기')),
-      body: CourseCard( // TODO: 코스 리스트 불러오는 로직 추가
-        course: dummyCourseData1,
-        isLiked: false,
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Row(
+              children: [
+                Expanded(
+                  // 검색창
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: '코스 이름 검색',
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide(color: MAIN_COLOR),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide(color: MAIN_COLOR),
+                      ),
+                    ),
+                    onChanged: (value) {
+                      _searchTitle = value;
+                      _filterCourses();
+                    },
+                  ),
+                ),
+                SizedBox(width: 5),
+
+                // 필터링 버튼
+                TextButton(
+                    onPressed: () {
+                      if (_isFilterApplied) {
+                        // 필터가 적용 중이라면 초기화
+                        setState(() {
+                          _selectedProvince = null;
+                          _selectedCity = null;
+                          _selectedTags = [];
+                          _isFilterApplied = false;
+                          _filterCourses(); // 필터 초기화 후 다시 필터링
+                        });
+                      } else {
+                        // 필터 모달을 열기
+                        _showFilterModal(context);
+                      }
+                    },
+                    style: TextButton.styleFrom(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      side: BorderSide(color: MAIN_COLOR),
+                      backgroundColor: _isFilterApplied ? MAIN_COLOR : Colors.white,
+                    ),
+                    child: Row(
+                      children: [
+                        Icon(Icons.filter_list, color: _isFilterApplied ? Colors.white : Colors.black),
+                        Text(_isFilterApplied ?
+                          '초기화' : '필터링',
+                          style: TextStyle(color: _isFilterApplied ? Colors.white : Colors.black),
+                        )
+                      ],
+                    )),
+              ],
+            ),
+          ),
+          // 필터링 된 코스 목록
+          Expanded(
+            child: ListView.builder(
+              itemCount: _filteredCourses.length,
+              itemBuilder: (context, index) {
+                final course = _filteredCourses[index];
+                return CourseCard(
+                  course: course,
+                  isLiked: false,
+                );
+              },
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => UploadCoursePage()
-            )
-          );
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => UploadCoursePage()));
         },
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
         backgroundColor: MAIN_COLOR,
---
+++ lib/widgets/filter_modal.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:pettrip_fe/const/style.dart';
+import '../widgets/province_city_selector.dart';
+import '../widgets/tag_selector.dart';
+import '../const/colors.dart';
+
+class FilterModal extends StatelessWidget {
+  final String? selectedProvince;
+  final String? selectedCity;
+  final List<String> selectedTags;
+  final List<String> tagList;
+  final ValueChanged<String?> onProvinceChanged;
+  final ValueChanged<String?> onCityChanged;
+  final ValueChanged<List<String>> onTagsChanged;
+  final VoidCallback onApply;
+
+  const FilterModal({
+    super.key,
+    this.selectedProvince,
+    this.selectedCity,
+    required this.selectedTags,
+    required this.onProvinceChanged,
+    required this.onCityChanged,
+    required this.onTagsChanged,
+    required this.onApply,
+    required this.tagList,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    String? tempProvince = selectedProvince;
+    String? tempCity = selectedCity;
+    List<String> tempTags = List.from(selectedTags);
+
+    return StatefulBuilder(
+      builder: (BuildContext context, StateSetter setState) {
+        return SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '지역',
+                  style: titleTextStyle,
+                ),
+                const SizedBox(height: 20),
+                // 지역 선택 위젯
+                ProvinceCitySelector(
+                  selectedProvince: tempProvince,
+                  selectedCity: tempCity,
+                  onProvinceChanged: (province) {
+                    setState((){
+                      tempProvince = province;
+                      tempCity = null;
+                      onProvinceChanged(province);
+                      onCityChanged(null);
+                    });
+                  },
+                  onCityChanged: (city) {
+                    setState((){
+                      tempCity = city;
+                      onCityChanged(city);
+                    });
+                  },
+                ),
+                const SizedBox(height: 30),
+                Text(
+                  '태그',
+                  style: titleTextStyle,
+                ),
+                // 태그 선택 위젯
+                TagSelector(
+                  initialSelectedTags: tempTags,
+                  tagList: tagList,
+                  onTagChanged: (tags) {
+                    tempTags = tags;
+                    onTagsChanged(tags);
+                  },
+                ),
+                const SizedBox(height: 16),
+                // 적용 버튼
+                Center(
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                      onApply(); // 필터 적용 로직 호출
+                    },
+                    style: defaultTextButtonStyle,
+                    child: Text('필터 적용', style: TextStyle(color: Colors.white)),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
