af0f927ac6856d3773309b5688ebdb2db1005bd6
gnvvoo <t412@naver.com>
2024-12-14 14:36:43 +0900

    Feat: Care Request 추가

--- lib/services/care_community_service.dart
+++ lib/services/care_community_service.dart
@@ -1,8 +1,14 @@
 import 'dart:convert';
+import 'package:dio/dio.dart';
+
 import 'package:http/http.dart' as http;
 import 'package:pettrip_fe/models/care_model.dart';
 
+import 'package:pettrip_fe/const/secret_key.dart';
+
 class CareCommunityService{
+  final Dio _dio = Dio(BaseOptions(baseUrl: backendUrl));
+
   static Future<List<CareModel>> fetchItems({String? title, String? location, String? breed}) async {
     final queryParameters = {
       //TODO: 필터 구현
@@ -25,7 +31,15 @@ class CareCommunityService{
       throw Exception("Failed to load data");
     }
   }
-
+  Future<void> addCareRequest(CareRequestDTO careRequestDTO) async {
+    try {
+      final response = await _dio.post('url',
+          data: careRequestDTO);
+    } catch (e) {
+      print('Error: $e');
+      throw Exception("댓글 서버 저장 실패: $e");
+    }
+  }
 }
 
 
